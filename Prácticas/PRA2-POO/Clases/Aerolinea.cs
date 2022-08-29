8b85ec3f2f5d5d8356b2df300191c3ce8be14660
pgaticac <>
2022-08-29 10:03:54 -0400

    Clase Avion y uso de Enum

---
+++ Prácticas/PRA2-POO/Clases/Aerolinea.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Aerolinea
+    {
+        List<Avion> flota = new List<Avion>();
+
+        public void Add(Avion a)
+        {
+            flota.Add(a);
+        }
+
+        public List<Avion> GetAll()
+        {
+            return flota.OrderBy(a => a.Año).ToList() ;
+        }
+
+        public List<Avion> GetByEstado(Estado e)
+        {
+            List<Avion> filtro = new List<Avion>();
+            foreach (Avion avion in flota)
+            {
+                if (avion.Estado == e)
+                {
+                    filtro.Add(avion);
+                }
+            }
+            return filtro;
+        }
+    }
+}
---
+++ Prácticas/PRA2-POO/Clases/Avion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Avion
+    {
+        private string _codigo;
+        private Estado _estado;
+        private int _año;
+
+        public Avion()
+        {
+            _estado = Estado.Disponible;
+        }
+
+        public Avion(string codigo, Estado estado, int año)
+        {
+            Codigo = codigo;
+            Estado = estado;
+            Año = año;
+        }
+
+        public string Codigo { get => _codigo; set => _codigo = value; }
+        public Estado Estado { get => _estado; set => _estado = value; }
+        public int Año { get => _año; set => _año = value; }
+
+
+    }
+
+    public enum Estado
+    {
+        Disponible,
+        Reparando,
+        Retirado
+    }
+}
